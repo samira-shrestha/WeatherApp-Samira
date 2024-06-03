b6cea09f0443d5f0afdb6ac934f537403ea19ad2
Samira Shrestha <samirashrestha24@gmail.com>
2024-06-03 07:41:38 +0545

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {Routes,Route} from "react-router-dom";
+import Header from "./components/header";
+import CurrentWeather from "./pages/currentWeather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<CurrentWeather/>}/>
+      <Route path="/current-weather" element={<CurrentWeather/>}/>
+      <Route path="/forecast" element={<>Hi</>}/>
+
+    </Routes>
+    </>
   );
 }
-
 export default App;
